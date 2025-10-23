1d87ae04e28bdff45d46a1c71d966deaf7b86e16
lsh02002 <lsh02002@hanmail.net>
2025-10-23 20:53:31 +0900

    Update BASE_URL.tsx

--- src/api/BASE_URL.tsx
+++ src/api/BASE_URL.tsx
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8110";
-// export const BASE_URL = "https://sehoproject.lsh02002.com";
+// export const BASE_URL = "http://localhost:8110";
+export const BASE_URL = "https://sehoproject.lsh02002.com";
