01ae2c41c1e62010f5822d91958cdd941058b1dc
lsh02002 <lsh02002@hanmail.net>
2026-05-05 13:30:33 +0900

    Update WorkspaceCreatePage.tsx

--- src/pages/workspace/WorkspaceCreatePage.tsx
+++ src/pages/workspace/WorkspaceCreatePage.tsx
@@ -21,7 +21,7 @@ const WorkspaceCreatePage = () => {
 
   const OnCreateSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation();
-    
+
     if (isSubmitting) return;
     setIsSubmitting(true);
 
@@ -59,10 +59,7 @@ const WorkspaceCreatePage = () => {
 
   return (
     <div className="d-flex justify-content-center align-items-center w-100 vh-100">
-      <div
-        className="w-100 p-4"
-        style={{ maxWidth: layout.maxWidth }}
-      >
+      <div className="w-100 p-4" style={{ maxWidth: layout.maxWidth }}>
         <div className="d-flex justify-content-between align-items-center mb-3">
           <div className="d-flex align-items-center gap-2">
             <span className="d-flex align-items-center">
